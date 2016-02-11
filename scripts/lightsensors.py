a3d5d77c5988c2b3f161431a2377ae55dcc88784
Yuki Iida <yuki_iida@cit-ar-lab.tech>
2016-02-12 04:17:24 +0900

    Add a test scripts

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -14,7 +14,7 @@ def get_freq():
  
     return f
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     devfile = '/dev/rtlightsensor0'
     rospy.init_node('lightsensors')
     pub = rospy.Publisher('lightsensors', LightSensorValues, queue_size=1)
@@ -23,20 +23,20 @@ if __name__ == "__main__":
     rate = rospy.Rate(freq)
     while not rospy.is_shutdown():
         try:
-           with open(devfile,'r') as f:
-               data = f.readline().split()
-               data = [ int(e) for e in data ]
-               d = LightSensorValues()
-               d.right_forward = data[0]
-               d.right_side = data[1]
-               d.left_side = data[2]
-               d.left_forward = [3]
-               d.sum_all = sum(data)
-               d.sum_forward = data[0] + data[3]
-               pub.publish(d)
-        except IOError:
-            rospy.logerr("cannot write to " + devfile)
-   
+            with open(devfile,'r') as f:
+                data = f.readline().split()
+                data = [ int(e) for e in data ]
+                d = LightSensorValues()
+                d.right_forward = data[0]
+                d.right_side = data[1]
+                d.left_side = data[2]
+                d.left_forward =data[3]
+                d.sum_all = sum(data)
+                d.sum_forward = data[0] + data[3]
+                pub.publish(d)
+        except:
+             rospy.logerr("cannot open" + devfile)
+
         f = get_freq()
         if f != freq:
             freq = f
